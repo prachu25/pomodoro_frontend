27a0cf7f800c145b4b09de4c89ded306c0ba546e
Prachi Gorde <gordeprachi64@gmail.com>
2026-02-11 17:17:18 +0530

    set login as landing page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/timer" element={<Timer />} />
